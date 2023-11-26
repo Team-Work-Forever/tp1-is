c12ac42625dddc406a8cf17ce3bfa48ceac5afc1
Diogo Assunção <dassuncao1234@gmail.com>
2023-11-26 18:20:52 +0000

    remove: Samples Directory

--- src/samples/xml-generation/main.py
+++
@@ -1,9 +0,0 @@
-from csv_to_xml_mine import CSVtoXMLMine
-from csv_to_xml_converter import CSVtoXMLConverter
-
-if __name__ == "__main__":
-    converter = CSVtoXMLConverter("/data/dataset.csv")
-    # converter = CSVtoXMLMine("/data/dataset.csv")
-
-    with open("/data/dataset.xml", "w") as file:
-        file.write(converter.to_xml_str())
