64bd75bb599f4726ae56ac681b92178506e48408
Diogo Assunção <dassuncao1234@gmail.com>
2023-11-26 00:05:54 +0000

    persist temp file

--- src/rpc-server/services/nominatim.py
+++ src/rpc-server/services/nominatim.py
@@ -3,27 +3,7 @@ import time
 
 class NominatimApi():
     BASE_URL = "https://nominatim.openstreetmap.org"
-    CACHE_MAP = {}
-    
-    def get_key(self, country, region):
-        return '-'.join([country, region])
-
-    def insert_region(self, country, region, point):
-        key = self.get_key(country, region)
-
-        if key not in NominatimApi.CACHE_MAP:
-            NominatimApi.CACHE_MAP[key] = [point]
-        else:
-            NominatimApi.CACHE_MAP[key].append(point)
-
-    def get_region(self, country, region):
-        key = self.get_key(country, region)
-
-        if key in NominatimApi.CACHE_MAP:
-            NominatimApi.CACHE_MAP[key]
-        
-        return None
-
+   
     async def _make_request(self, path: str):
         async with httpx.AsyncClient() as client:
             response = await client.get(self.BASE_URL + path)
@@ -32,11 +12,6 @@ class NominatimApi():
         return response.json()
 
     async def get_value(self, country: str, region: str) -> (str, str):
-        point = self.get_region(country, region)
-
-        if point is not None:
-            return point
-
         try:
             response = await self._make_request(f"/search?country={country}&city={region}&format=json")
 
@@ -44,8 +19,6 @@ class NominatimApi():
                 return 0, 0
             
             result = str(response[0]['lat']), str(response[0]['lon'])
-
-            self.insert_region(country, region, result)
 
             time.sleep(1)
             return result
--- src/rpc-server/services/worker.py
+++ src/rpc-server/services/worker.py
@@ -11,25 +11,52 @@ class NominatimWorker(Process):
 
     def __init__(self, countries, regions, root_el: ET, converter) -> None:
         super().__init__()
+        NominatimWorker.CACHE_MAP = {}
 
         self._db_connection = DbConnection()
         self._countries = countries
         self._regions = regions
         self._root_el = root_el
         self._converter = converter
+     
+    @staticmethod
+    def get_key(country, region):
+        return '-'.join([country, region])
+
+    @staticmethod
+    def insert_region(country, region, point):
+        key = NominatimWorker.get_key(country, region)
+
+        if key not in NominatimWorker.CACHE_MAP:
+            NominatimWorker.CACHE_MAP[key] = [point]
+        else:
+            NominatimWorker.CACHE_MAP[key].append(point)
+
+    @staticmethod
+    def get_region(country, region):
+        key = NominatimWorker.get_key(country, region)
+
+        if key in NominatimWorker.CACHE_MAP:
+            NominatimWorker.CACHE_MAP[key]
+        
+        return None
 
     async def run_request(self):
         for country in self._countries.values():
             for region in self._regions.values():
                 if country.get_id() == region.get_country_id():
-                    api_request = await NominatimWorker.NOMINATIM_API.get_value(country.get_name(), region.get_region())
+                    point = NominatimWorker.get_region(country.get_name(), region.get_region())
+
+                    if point is not None:
+                       api_request = point
+                    else:
+                        api_request = await NominatimWorker.NOMINATIM_API.get_value(country.get_name(), region.get_region())
+                        NominatimWorker.insert_region(country.get_name(), region.get_region(), api_request)
+
                     region_el = self._root_el.find(".//Region[@id='" + region.get_id() + "']")
                     region_el.set("lat", str(api_request[0]))
                     region_el.set("lon", str(api_request[1]))
-                    print()
-                    print(f"Country: {country.get_name()}, Region: {region.get_region()}")
-                    print(f"lat: {api_request[0]}, lon: {api_request[1]}")
-
+                    
     def run(self) -> None:
         asyncio.run(self.run_request())
         result = self._converter.to_xml_str(self._root_el)
